de3c16e755fb38fb44df483b0bf71c89419f8643
skia3649 <skia3649@gmail.com>
2017-08-06 22:25:42 +0900

    success problems#index problems#show

---
+++ app/controllers/problems_controller.rb
@@ -0,0 +1,10 @@
+class ProblemsController < ApplicationController
+  def index
+    @problems = Problem.all
+  end
+  def show
+    @problem = Problem.find(params[:id])
+    @patterns = @problem.patterns
+
+  end
+end
